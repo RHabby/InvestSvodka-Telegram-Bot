97b51aa2b2cc393a9355cb0abbe929b9f30ac947
Ravil Khabibullin <ravil.khabibullin94@gmail.com>
2021-08-09 11:59:29 +0300

    main config file

---
+++ tg_bot/config.py
@@ -0,0 +1,53 @@
+import configparser
+from dataclasses import dataclass
+
+
+@dataclass
+class TgBot:
+    """tgbot credentials"""
+
+    token: str
+    admin_id: int
+    use_redis: bool
+
+
+@dataclass
+class DbConfig:
+    """database credentials"""
+
+    user: str
+    password: str
+    host: str
+    database: str
+
+
+@dataclass
+class Config:
+    """main config class"""
+
+    tg_bot: TgBot
+    db: DbConfig
+
+
+def cast_bool(value: str) -> bool:
+    """cast boolean value"""
+    if not value:
+        return False
+    return value.lower() in ("true", "t", "1", "yes")
+
+
+def load_config(path: str) -> Config:
+    """read and load config function"""
+    config = configparser.ConfigParser()
+    config.read(path)
+
+    tg_bot = config["tg_bot"]
+
+    return Config(
+        tg_bot=TgBot(
+            token=tg_bot["token"],
+            admin_id=int(tg_bot["admin_id"]),
+            use_redis=cast_bool(tg_bot["use_redis"]),
+        ),
+        db=DbConfig(**config["db"]),
+    )
